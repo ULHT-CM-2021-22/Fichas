891dd5c7d453a77c1180bc4643b3c297a05342a8
Miguel Tavares <hmigueltavares@gmail.com>
2022-04-28 10:55:55 +0100

    added delete

--- app/src/main/java/com/github/mstavares/cm/fichas/CalculatorService.kt
+++ app/src/main/java/com/github/mstavares/cm/fichas/CalculatorService.kt
@@ -1,14 +1,13 @@
 package com.github.mstavares.cm.fichas
 
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 
 data class PostOperationRequest(val expression: String, val result: Double, val timestamp: Long)
 data class PostOperationResponse(val message: String)
 
 data class GetOperationsResponse(val uuid: String, val expression: String, val result: Double, val timestamp: Long)
+
+data class DeleteOperationByIdResponse(val message: String)
 
 interface CalculatorService {
 
@@ -19,5 +18,9 @@ interface CalculatorService {
     @Headers("apikey: 8270435acfead39ccb03e8aafbf37c49359dfbbcac4ef4769ae82c9531da0e17")
     @GET("operations")
     suspend fun getAll(): List<GetOperationsResponse>
+
+    @Headers("apikey: 8270435acfead39ccb03e8aafbf37c49359dfbbcac4ef4769ae82c9531da0e17")
+    @DELETE("operations/{uuid}")
+    suspend fun deleteById(@Path("uuid") uuid: String): DeleteOperationByIdResponse
 
 }
